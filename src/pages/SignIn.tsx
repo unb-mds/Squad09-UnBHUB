224eda968063e99077870d9d9c0d81f118eb1a28
Túlio Celeri <celeritulio@terra.com.br>
2024-04-12 19:44:21 -0300

    Feat: Criando componente SignInCard

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,55 +1,15 @@
-import { Button } from 'primereact/button';
-import InputComponent from '../components/Input';
-import CheckboxComponent from '../components/Checkbox';
+import SignInCardComponent from '../components/SignInCard';
 
 export default function SignInScreens() {
   return (
     <div>
-      <div className="flex align-items-center justify-content-center">
-        <div className="surface-card p-4 shadow-2 border-round lg:w-4">
-          <div className="text-center mb-5">
-            <div className="text-900 text-3xl font-medium mb-3">
-              Entre no UnBHUB
-            </div>
-            <div>Conecte-se para explorar sua vida univerditária</div>
-          </div>
-          <div>
-            <InputComponent label="Endereço de E-mail" />
-            <InputComponent label="Senha" />
-            <div className="flex align-items-center justify-content-between mb-6">
-              <div className="flex align-items-center">
-                <CheckboxComponent />
-              </div>
-              <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">
-                Esqueceu sua senha?
-              </a>
-            </div>
-
-            <Button
-              className="py-3 px-8 w-full text-white my-2"
-              label="Entrar"
-            />
-            <span className="text-600 font-medium line-height-3">
-              Ainda não tem uma conta?
-            </span>
-            <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">
-              Cadastrar-se
-            </a>
-          </div>
-        </div>
-      </div>
+      <div
+        className="flex bg-center w-full h-screen absolute"
+        style={{
+          backgroundImage: "url('public/images/imagemlogin.jpg')",
+        }}
+      ></div>
+      <SignInCardComponent />
     </div>
   );
-}
-
-// Código para adicionar imagem de fundo
-{
-  /* <div className="overflow-hidden">
-  <div
-    className="bg-cover bg-center border-primary-500 border-2 border-round h-20rem w-full h-20rem"
-    style={{
-      backgroundImage: "url('public/images/imagemlogin.jpg')",
-    }}
-  ></div>
-</div>; */
 }
