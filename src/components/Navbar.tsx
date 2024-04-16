61e06bf0924a9acae09da24dca32e1e6f0049870
Felipe Ribeiro <143733331+FelipeRibeirooo@users.noreply.github.com>
2024-04-15 21:25:55 -0300

    Rascunho inicial do Dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ import { InputText } from 'primereact/inputtext';
 
 export default function Navbar() {
   return (
-    <div className="flex ">
+    <div className="flex justify-content-between ">
       <div>
         <InputText type="text" placeholder="Buscar"></InputText>
       </div>
-      <div className="absolute top-0 right-0 flex align-items-center justify-content-center gap-4  w-20rem h-4rem border-round">
+      <div className="gap-5">
         <i className="pi pi-bell p-overlay-badge" style={{ fontSize: '2rem' }}>
           <Badge></Badge>
         </i>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import SideBarComponent from '../components/SideBar';
 import Navbar from '../components/Navbar';
+import { ProgressBar } from 'primereact/progressbar';
+import { Button } from 'primereact/button';
+import CalendarComponent from '../components/Calendar';
+import { Card } from 'primereact/card';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 export default function DashboardScreen() {
   return (
@@ -9,7 +15,125 @@ export default function DashboardScreen() {
       <div>
         <Outlet />
       </div>
-      <Navbar></Navbar>
+      <div className="flex flex-column">
+        <Navbar></Navbar>
+        <div className="flex">
+          <h1>👋 Bem-Vindo!</h1>
+          <div className="w-3 absolute right-0 pt-4">
+            <ProgressBar value={50}></ProgressBar>
+          </div>
+        </div>
+        <div className="flex flex-column w-8">
+          <div>
+            <h2>
+              <b>Envolva-se-Entre em Grupos de Estudos e Monitoria!</b>
+            </h2>
+            <h3>
+              Explore seus interesses e conheça estudantes que pensam como você
+              ingressando em um de nossos muitos clubes. Quer você goste de
+              esportes, artes ou estudos, há um clube para você. Encontre sua
+              comunidade!
+            </h3>
+            <Button
+              label="Saiba Mais!"
+              icon="pi pi-arrow-right"
+              iconPos="right"
+              size="small"
+            />
+          </div>
+
+          <div className="w-3 absolute right-0">
+            <CalendarComponent />
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <div className="w-8 flex justify-content-between">
+            <p>Matérias</p>
+            <Button
+              label="Ver Tudo"
+              icon="pi pi-angle-right"
+              iconPos="right"
+              size="small"
+              text
+            />
+          </div>
+
+          <div className="w-4 flex justify-content-between">
+            <p>Tarefas</p>
+            <Button
+              label="Ver Tudo"
+              icon="pi pi-angle-right"
+              iconPos="right"
+              size="small"
+              text
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-row">
+          <div className="flex flex-column w-8">
+            <div className="w-8 flex flex-row justify-content-between gap-8">
+              <Card title="Matéria 01">
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </Card>
+              <Card title="Matéria 02">
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </Card>
+              <Card title="Matéria 03">
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </Card>
+              <Card title="Matéria 04">
+                <p className="m-0">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                </p>
+              </Card>
+            </div>
+            <div className="w-8 flex justify-content-between">
+              <p>Provas</p>
+              <Button
+                label="Ver Tudo"
+                icon="pi pi-angle-right"
+                iconPos="right"
+                size="small"
+                text
+              />
+            </div>
+            <DataTable tableStyle={{ minWidth: '50rem' }}>
+              <Column field="subjects" header="Matérias"></Column>
+              <Column field="course" header="Curso"></Column>
+              <Column field="date" header="Data"></Column>
+              <Column field="time" header="Horário"></Column>
+              <Column field="room" header="Sala"></Column>
+              <Column field="status" header="Status"></Column>
+            </DataTable>
+          </div>
+
+          <div className="pl-3 w-4 flex-column ">
+            <Card title="Tarefa 01">
+              <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </Card>
+            <Card title="Tarefa 02">
+              <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </Card>
+            <Card title="Tarefa 03">
+              <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+            </Card>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
