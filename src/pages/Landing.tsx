5197d66ea105dfe38b470500d219e4582e56edb4
Túlio Celeri <celeritulio@terra.com.br>
2024-05-23 16:48:08 -0300

    Criando a branch da LandingPage

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+export default function LandingPage() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam,
+      aspernatur! Illum fugit a quas ex sed inventore excepturi provident
+      adipisci vero laboriosam deleniti, accusantium sint facilis neque
+      voluptatem minima eos.
+    </div>
+  );
+}
