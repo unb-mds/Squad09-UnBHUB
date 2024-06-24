db359715d3e2d71476989d7666c76a7ca73e83de
Lucca Medeiros <luccameds@gmail.com>
2024-06-24 19:17:35 -0300

    Feat: Added Formik and Yup in SignUp.

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,15 +1,81 @@
-import SignUpCardComponent from '../components/SignUpCard';
+import { Formik } from 'formik';
+import { Button } from 'primereact/button';
 
-export default function SignUpScreen() {
+import * as Yup from 'yup';
+import InputComponent from '../components/Input';
+
+export default function SignUpCardComponent() {
+  const schema = Yup.object().shape({
+    name: Yup.string()
+      .min(3, 'Username must be at least 3 characters long')
+      .required('Required'),
+    email: Yup.string()
+      .email('Please enter a valid email')
+      .required('Required'),
+    Senha: Yup.string()
+      .min(6, 'Password must be at least 6 characters long')
+      .required('Required'),
+    SenhaNovamente: Yup.string()
+      .oneOf([Yup.ref('Senha'), ''], 'Passwords must match')
+      .required('Required'),
+  });
+
+  const handleSubmit = () => {};
+
   return (
-    <div className="flex">
-      <SignUpCardComponent />
-      <div
-        className="h-screen w-full bg-center bg-cover hidden lg:block  relative object-contain"
-        style={{
-          backgroundImage: "url('/images/background-2.jpg')",
-        }}
-      ></div>
-    </div>
+    <Formik
+      validationSchema={schema}
+      onSubmit={handleSubmit}
+      initialValues={{
+        name: '',
+        email: '',
+        Senha: '',
+        SenhaNovamente: '',
+      }}
+    >
+      {({ handleChange, handleSubmit, values, errors }) => (
+        <div className="flex flex-column surface-card p-6 shadow-2 relative w-full border-round lg:w-5 h-screen justify-content-center">
+          <div className="text-center mb-5">
+            <div className="text-900 text-3xl font-medium mb-3">
+              Crie sua conta e conecte-se!
+            </div>
+            <div>Registro gratuito para começar a aproveitar! </div>
+          </div>
+          <div className="flex gap-2 flex-column">
+            <InputComponent
+              label="Endereço de E-mail"
+              value={values.email}
+              setValue={handleChange('email')}
+            />
+            {errors.email ? <div>{errors.email}</div> : null}
+
+            <InputComponent
+              label="Username"
+              value={values.name}
+              setValue={handleChange('name')}
+            />
+            {errors.name ? <div>{errors.name}</div> : null}
+            <InputComponent
+              label="Senha"
+              value={values.Senha}
+              setValue={handleChange('Senha')}
+            />
+            {errors.Senha ? <div>{errors.Senha}</div> : null}
+            <InputComponent
+              label="Senha novamente"
+              value={values.SenhaNovamente}
+              setValue={handleChange('SenhaNovamente')}
+            />
+            {errors.SenhaNovamente ? <div>{errors.SenhaNovamente}</div> : null}
+            <div className="flex align-items-center justify-content-between mb-6"></div>
+            <Button
+              onClick={() => handleSubmit()}
+              className="py-3 px-8 w-full text-white my-0"
+              label="Cadastre-se"
+            />
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 }
