83e2ee36d404aeb6399c0eee6e715eef7e48d7d5
Felipe Ribeiro <143733331+FelipeRibeirooo@users.noreply.github.com>
2024-07-08 19:24:06 -0300

    Estilização da página subjects. Co-authored-by: Felipe Ribeiro <felipebsb2010@gmail.com> Co-authored-by: Ana Carolina <anacarolinamfialho@gmail.com>

--- src/components/SubjectsComponents.tsx
+++ src/components/SubjectsComponents.tsx
@@ -1,14 +1,15 @@
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
 import { Avatar } from 'primereact/avatar';
+import { Divider } from 'primereact/divider';
 
 export default function SubjectsComponent(props: {
   setVisible: (visible: boolean) => void;
 }) {
   return (
-    <div className="flex flex-column mx-3 my-3 gap-2 w-full">
-      <div className="flex align-items-center justify-content-between surface-50 border-round-lg">
-        <div className="flex h-1rem gap-2 align-items-center surface-50 px-6 py-5">
+    <div className="flex flex-column mx-3 my-3 w-full">
+      <div className="flex align-items-center justify-content-between border-round-lg">
+        <div className="flex h-1rem gap-2 align-items-center px-6 py-5">
           <i className="pi pi-book text-4xl"> </i>
           <h1>Matérias</h1>
         </div>
@@ -44,8 +45,11 @@ export default function SubjectsComponent(props: {
         </div>
       </div>
 
-      <div className="flex h-3rem gap-2 justify-content-between align-items-center surface-50 px-6 border-round-lg">
-        Em Andamento
+      <div className="flex justify-content-between align-items-center px-6">
+        <div>
+          <i className="pi pi-forward mx-3" style={{ color: '#3498db' }} />
+          Em Andamento
+        </div>
         <a>
           <Button
             label="Adicionar"
@@ -58,9 +62,17 @@ export default function SubjectsComponent(props: {
           />
         </a>
       </div>
+      <Divider className="mb-4"></Divider>
 
       <div className="flex flex-row justify-content-between gap-2">
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #3498db',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -68,7 +80,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #3498db',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -76,7 +95,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #3498db',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -84,7 +110,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #3498db',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -94,12 +127,23 @@ export default function SubjectsComponent(props: {
         </Card>
       </div>
 
-      <div className="flex h-3rem align-items-center surface-50 px-6 border-round-lg">
-        Finalizadas
+      <div className="flex mt-3 align-items-center px-6">
+        <div>
+          <i className="pi pi-check mb-2 mx-3" style={{ color: 'green' }} />
+          Finalizadas
+        </div>
       </div>
+      <Divider className="mb-4"></Divider>
 
       <div className="flex flex-row justify-content-between gap-2">
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #12e42b',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -107,7 +151,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #12e42b',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -115,7 +166,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #12e42b',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
@@ -123,42 +181,14 @@ export default function SubjectsComponent(props: {
             <p className="pi pi-map-marker">LAB MOCAP</p>
           </div>
         </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
-          <div className="flex flex-column w-12">
-            <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
-            <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
-            <p className="pi pi-clock mb-2">10:00 - 11:50</p>
-            <p className="pi pi-map-marker">LAB MOCAP</p>
-          </div>
-        </Card>
-      </div>
-
-      <div className="flex flex-row justify-content-between gap-2">
-        <Card title="FGA0138 - MDS" className="w-12">
-          <div className="flex flex-column w-12">
-            <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
-            <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
-            <p className="pi pi-clock mb-2">10:00 - 11:50</p>
-            <p className="pi pi-map-marker">LAB MOCAP</p>
-          </div>
-        </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
-          <div className="flex flex-column w-12">
-            <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
-            <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
-            <p className="pi pi-clock mb-2">10:00 - 11:50</p>
-            <p className="pi pi-map-marker">LAB MOCAP</p>
-          </div>
-        </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
-          <div className="flex flex-column w-12">
-            <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
-            <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
-            <p className="pi pi-clock mb-2">10:00 - 11:50</p>
-            <p className="pi pi-map-marker">LAB MOCAP</p>
-          </div>
-        </Card>
-        <Card title="FGA0138 - MDS" className="w-12">
+        <Card
+          title="FGA0138 - MDS"
+          className="w-12"
+          style={{
+            color: 'white',
+            border: '2px solid #12e42b',
+          }}
+        >
           <div className="flex flex-column w-12">
             <p className="pi pi-user mt-0 mb-2">Carla Rocha</p>
             <p className="pi pi-calendar mb-2">Quarta & Sexta</p>
