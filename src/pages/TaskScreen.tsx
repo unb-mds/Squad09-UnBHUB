e8c7312914db87c8edee3fd499c140e270c3bf9d
Antonio Carvalho <antoniosousac2003@gmail.com>
2024-07-11 20:17:22 -0300

    [Feat]: Começando a criação do formulário de criação de atividade.

--- src/pages/TaskScreen.tsx
+++ src/pages/TaskScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import ActivitiesComponent from '../components/ActivitiesComponent';
 import SideBarComponent from '../components/SideBar';
+import CreateActivityComponent from '../components/Activities/createActivity';
 
 export default function Activities() {
+  const [visible, setVisible] = useState<boolean>(false);
   return (
     <div className="flex flex-row">
-      <SideBarComponent /> <ActivitiesComponent />{' '}
+      <SideBarComponent /> <ActivitiesComponent />
+      {''}
+      <CreateActivityComponent visible={visible} setVisible={setVisible} />
     </div>
   );
 }
