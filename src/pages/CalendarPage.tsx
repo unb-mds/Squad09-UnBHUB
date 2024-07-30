cc3c90da6dc9e563b1b3d72e0e7fff7487c535db
Túlio Celeri <celeritulio@terra.com.br>
2024-07-30 20:30:57 -0300

    Feat(#80): Finalizando a estilização da página de calendário
    Adicionar Descrição das Datas: Incluir descrições personalizadas para cada data no calendário.
    
    Fazer Footer com a URL: Adicionar um rodapé ao calendário que inclua uma URL específica, facilitando o acesso a informações complementares.

--- src/pages/CalendarPage.tsx
+++ src/pages/CalendarPage.tsx
@@ -1,6 +1,8 @@
 import SideBarComponent from '../components/SideBar';
 import NavbarComponent from '../components/Navbar';
 import CalendarComp from '../components/CalendarComponent';
+import Index from '../components/CalendarIndex';
+import Footer from '../components/CalendarFooter';
 
 export default function CalendarScreen() {
   return (
@@ -8,7 +10,16 @@ export default function CalendarScreen() {
       <SideBarComponent />
       <div className="flex flex-column w-full">
         <NavbarComponent />
-        <CalendarComp />
+        <div className="flex justify-content-between align-items-center surface-50 my-1 mx-3 border-round-xl">
+          <div className="flex flex-column justify-content-center w-12 px-6">
+            <h1 className="text-center mb-4">📅 Calendário Acadêmico</h1>
+            <div className="mb-4">
+              <CalendarComp />
+            </div>
+            <Index />
+          </div>
+        </div>
+        <Footer />
       </div>
     </div>
   );
