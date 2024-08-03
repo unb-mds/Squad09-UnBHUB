8ee671247c8bbfb2c6efe40a1ae99e3e85c7b089
Túlio Celeri <celeritulio@terra.com.br>
2024-08-03 19:59:30 -0300

    Feat(#95): Exibir as datas de provas no calendário
    Exibição das datas de provas no calendário na cor vermelha

--- src/components/Calendar/CalendarComponent.tsx
+++ src/components/Calendar/CalendarComponent.tsx
@@ -1,11 +1,12 @@
 import { Calendar, CalendarDateTemplateEvent } from 'primereact/calendar';
 import { Nullable } from 'primereact/ts-helpers';
 import { useEffect, useState } from 'react';
-import { fetchEvents } from '../../functions/Calendar/EventService'; // Importa a função do serviço
+import { fetchEvents } from '../../functions/Calendar/EventService';
+import { fetchExamDates } from '../../functions/Calendar/fetchExamDates'; // Importa a função do serviço
 import {
   getHighlightedMessage,
   isHighlighted,
-} from '../../functions/Calendar/HighlightUtils'; // Importa funções utilitárias
+} from '../../functions/Calendar/HighlightUtils';
 
 export default function CalendarComp() {
   const [date, setDate] = useState<Nullable<Date>>(null);
@@ -13,6 +14,9 @@ export default function CalendarComp() {
   const [highlightedMessages, setHighlightedMessages] = useState<
     Record<string, string>
   >({});
+  const [examDates, setExamDates] = useState<
+    { date: string; codeSubject: string }[]
+  >([]);
 
   useEffect(() => {
     const fetchAndSetEvents = async () => {
@@ -21,19 +25,45 @@ export default function CalendarComp() {
       setHighlightedMessages(messages);
     };
 
+    const fetchAndSetExamDates = async () => {
+      try {
+        const dates = await fetchExamDates();
+        setExamDates(dates);
+        console.log('Exam dates set:', dates); // Adicione este log
+      } catch (error) {
+        console.error('Error fetching exam dates:', error);
+      }
+    };
+
     fetchAndSetEvents();
+    fetchAndSetExamDates();
   }, []);
+
+  const isExamDate = (date: Date): string => {
+    const formattedDate = date.toLocaleDateString();
+    const examDate = examDates.find((d) => d.date === formattedDate);
+    return examDate ? 'red' : '';
+  };
 
   const dateTemplate = (event: CalendarDateTemplateEvent) => {
     const { day, year, month } = event;
     const date = new Date(year, month, day);
     const isDateHighlighted = isHighlighted(date, highlightedDates);
     const message = getHighlightedMessage(date, highlightedMessages);
+    const examColor = isExamDate(date);
+
+    const className = isDateHighlighted
+      ? examColor
+        ? 'highlighted-date exam-date'
+        : 'highlighted-date'
+      : examColor
+      ? 'exam-date'
+      : '';
 
     return (
       <div
-        className={isDateHighlighted ? 'highlighted-date' : ''}
-        title={message}
+        className={className}
+        title={message || (examColor ? 'Data de Prova' : '')}
       >
         {day}
       </div>
@@ -53,6 +83,17 @@ export default function CalendarComp() {
         {`
           .highlighted-date {
             background-color: #ffd700;
+            border-radius: 50%;
+            color: white;
+            padding: 5px;
+            width: 2em;
+            height: 2em;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+          .exam-date {
+            background-color: red;
             border-radius: 50%;
             color: white;
             padding: 5px;
