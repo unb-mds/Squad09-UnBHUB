111183dbf775283258d8276e85c97c557fe625e4
anawcarol <122827734+anawcarol@users.noreply.github.com>
2024-08-06 20:27:13 -0300

    Update: melhorias no componente de edição provas

---
+++ src/components/Subject/EditExamDialog.tsx
@@ -0,0 +1,212 @@
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import { Calendar } from 'primereact/calendar';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from '../../../config/firebase';
+import DeleteExam from '../../functions/DeleteExam';
+import EditExam from '../../functions/EditExam'; // Importa a função EditExam
+
+interface EditExamDialogProps {
+  visible: boolean;
+  onHide: () => void;
+  exam: {
+    code: string;
+    score: string;
+    date: Date;
+    time: Date;
+    room: string;
+    status: string;
+    id: string; // ID da prova para identificar o documento no Firestore
+  };
+  subjectId: string; // ID da matéria para identificar o documento no Firestore
+}
+
+export default function EditExamDialog({
+  visible,
+  onHide,
+  exam,
+  subjectId,
+}: EditExamDialogProps) {
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={{
+        code: exam.code,
+        score: exam.score,
+        date: exam.date,
+        time: exam.time,
+        room: exam.room,
+        status: exam.status,
+      }}
+      validationSchema={Yup.object().shape({
+        code: Yup.string().required('O código da prova é obrigatório'),
+        score: Yup.string().required('A nota é obrigatória'),
+        date: Yup.date().required('A data é obrigatória'),
+        time: Yup.date().required('O horário é obrigatório'),
+        room: Yup.string().required('A sala é obrigatória'),
+        status: Yup.string().required('O status é obrigatório'),
+      })}
+      onSubmit={async (values) => {
+        const formattedValues = {
+          ...values,
+          date: new Date(values.date),
+          time: new Date(values.time),
+        };
+
+        // Atualiza o exame
+        await EditExam(formattedValues, exam.id, subjectId);
+        onHide(); // Fechar o diálogo após a atualização
+      }}
+    >
+      {({
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        setFieldValue,
+        errors,
+        touched,
+      }) => (
+        <form
+          onSubmit={handleSubmit}
+          className="card flex justify-content-center gap-6"
+        >
+          <Dialog
+            header="Editar Prova"
+            visible={visible}
+            style={{ width: '30vw' }}
+            onHide={onHide}
+          >
+            <FloatLabel>
+              <InputText
+                className="flex mt-5 mb-5 w-full"
+                id="code"
+                name="code"
+                value={values.code}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <label htmlFor="code">Código</label>
+            </FloatLabel>
+            {errors.code && touched.code ? (
+              <div className="text-red-500">{errors.code}</div>
+            ) : null}
+
+            <FloatLabel>
+              <InputText
+                className="flex mt-5 mb-5 w-full"
+                id="score"
+                name="score"
+                value={values.score}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <label htmlFor="score">Nota</label>
+            </FloatLabel>
+            {errors.score && touched.score ? (
+              <div className="text-red-500">{errors.score}</div>
+            ) : null}
+
+            <FloatLabel>
+              <label htmlFor="date">Data</label>
+              <Calendar
+                className="flex w-full"
+                id="date"
+                name="date"
+                value={values.date}
+                onChange={(e) => setFieldValue('date', e.value)}
+                showIcon
+                dateFormat="dd/mm/yy"
+              />
+            </FloatLabel>
+            {errors.date && touched.date ? (
+              <div className="text-red-500">{errors.date}</div>
+            ) : null}
+
+            <FloatLabel>
+              <label htmlFor="time">Horário</label>
+              <Calendar
+                className="flex w-full mt-5"
+                id="time"
+                name="time"
+                value={values.time}
+                onChange={(e) => setFieldValue('time', e.value)}
+                icon={() => <i className="pi pi-clock" />}
+                showIcon
+                timeOnly
+              />
+            </FloatLabel>
+            {errors.time && touched.time ? (
+              <div className="text-red-500">{errors.time}</div>
+            ) : null}
+
+            <FloatLabel>
+              <InputText
+                className="flex mt-5 mb-5 w-full"
+                id="room"
+                name="room"
+                value={values.room}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <label htmlFor="room">Sala</label>
+            </FloatLabel>
+            {errors.room && touched.room ? (
+              <div className="text-red-500">{errors.room}</div>
+            ) : null}
+
+            <FloatLabel>
+              <InputText
+                className="flex mt-5 mb-5 w-full"
+                id="status"
+                name="status"
+                value={values.status}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+              <label htmlFor="status">Status</label>
+            </FloatLabel>
+            {errors.status && touched.status ? (
+              <div className="text-red-500">{errors.status}</div>
+            ) : null}
+
+            <div className="flex justify-content-between gap-3">
+              <Button
+                outlined
+                label="Finalizar"
+                onClick={async () => {
+                  const formattedValues = {
+                    ...values,
+                    status: 'Finalizado',
+                  };
+
+                  // Atualiza o exame
+                  await EditExam(formattedValues, exam.id, subjectId);
+                  onHide(); // Fechar o diálogo após a atualização
+                }}
+              />
+              <Button
+                outlined
+                label="Excluir"
+                style={{
+                  borderColor: '#ff6060',
+                  color: '#ff6060',
+                }}
+                onClick={async () => {
+                  console.log(exam.id);
+                  await DeleteExam(subjectId, exam.id);
+                  onHide(); // Fechar o diálogo após a atualização
+                }}
+              />
+              <Button onClick={handleSubmit} label="Confirmar" />
+            </div>
+          </Dialog>
+        </form>
+      )}
+    </Formik>
+  );
+}
