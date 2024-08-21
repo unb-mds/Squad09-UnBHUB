fed607226ceca983d0267ad3fc18d4c77b2968d6
Felipe Ribeiro <143733331+FelipeRibeirooo@users.noreply.github.com>
2024-08-21 11:04:40 -0300

    Feat: Atualiza os status de tarefas e provas quando uma matéria for finalizada ou deletada

--- src/functions/Subjects/FinalizeSubject.ts
+++ src/functions/Subjects/FinalizeSubject.ts
@@ -1,11 +1,37 @@
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { auth, db } from '../../../config/firebase';
+import FinalizeActivityFunction from '../FinalizedActivity';
+import FinalizeExamFunction from './FinalizeExam';
 
 export default async function FinalizeSubjectFunction(id: string) {
   if (!auth.currentUser) return;
 
   const subjectRef = doc(db, 'Users', auth.currentUser.uid);
-  await updateDoc(subjectRef, {
-    [`subjects.${id}.status`]: 'Finalized',
-  });
+  const subjectSnapshot = await getDoc(subjectRef);
+
+  if (subjectSnapshot.exists()) {
+    const tasks = subjectSnapshot.get(`subjects.${id}.tasks`);
+    const exams = subjectSnapshot.get(`subjects.${id}.exams`);
+
+    // Finalizar cada tarefa
+    if (tasks) {
+      const taskIds = Object.keys(tasks);
+      for (const taskId of taskIds) {
+        await FinalizeActivityFunction(id, taskId);
+      }
+    }
+
+    // Finalizar a matéria
+    await updateDoc(subjectRef, {
+      [`subjects.${id}.status`]: 'Finalized',
+    });
+
+    // Finalizar cada exame
+    if (exams) {
+      const examIds = Object.keys(exams);
+      for (const examId of examIds) {
+        await FinalizeExamFunction(id, examId);
+      }
+    }
+  }
 }
--- src/functions/Subjects/ReturnToActiveSubject.ts
+++ src/functions/Subjects/ReturnToActiveSubject.ts
@@ -1,11 +1,26 @@
-import { doc, updateDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { auth, db } from '../../../config/firebase';
 
-export default async function FinalizeSubjectFunction(id: string) {
+export default async function ReactivateSubjectFunction(id: string) {
   if (!auth.currentUser) return;
 
   const subjectRef = doc(db, 'Users', auth.currentUser.uid);
-  await updateDoc(subjectRef, {
-    [`subjects.${id}.status`]: 'Active',
-  });
+  const subjectSnapshot = await getDoc(subjectRef);
+
+  if (subjectSnapshot.exists()) {
+    const tasks = subjectSnapshot.get(`subjects.${id}.tasks`);
+
+    await updateDoc(subjectRef, {
+      [`subjects.${id}.status`]: 'Active',
+    });
+
+    if (tasks) {
+      const taskIds = Object.keys(tasks);
+      for (const taskId of taskIds) {
+        await updateDoc(doc(db, 'Users', auth.currentUser.uid), {
+          [`subjects.${id}.tasks.${taskId}.status`]: 'Active',
+        });
+      }
+    }
+  }
 }
