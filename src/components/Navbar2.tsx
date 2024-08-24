287f82c780bec928f18a009d94cd6adcaa9332e3
Túlio Celeri <celeritulio@terra.com.br>
2024-08-24 16:06:16 -0300

    Fix(#131): Consertar responsividade dos botões e padronização do footer na LandingPage

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -4,16 +4,24 @@ import { Image } from 'primereact/image';
 export default function NavbarComponent2() {
   return (
     <div className="flex justify-content-between align-items-center surface-50">
-      <div className="flex justify-content-between w-9 px-6">
+      <div
+        className="flex align-items-center w-3"
+        style={{ marginLeft: '30px' }}
+      >
         <Image src="/images/logo.svg" width="100" height="100" alt="UnBHUB" />
       </div>
-      <div className="flex align-items-center">
+      <div className="flex align-items-center justify-content-end w-9 flex-nowrap">
         <a href="http://localhost:5173/SignIn" className="mx-2">
-          <Button className="py-3 px-5 w-full text-white " label="Entrar" />
+          <Button
+            style={{ minWidth: '120px', whiteSpace: 'nowrap' }}
+            className="py-3 px-5 text-white"
+            label="Entrar"
+          />
         </a>
         <a href="http://localhost:5173/signUp" className="mx-2">
           <Button
-            className="py-3 px-5 w-full text-white "
+            style={{ minWidth: '120px', whiteSpace: 'nowrap' }}
+            className="py-3 px-5 text-white"
             label="Cadastre-se"
           />
         </a>
