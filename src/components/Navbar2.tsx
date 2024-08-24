704dcb70a2a58bd5d0d8c0c48911580984a988e4
Túlio Celeri <celeritulio@terra.com.br>
2024-08-24 16:17:21 -0300

    Fix(#131): Evitar o overflow dos botões da landing page

--- src/components/Navbar2.tsx
+++ src/components/Navbar2.tsx
@@ -3,7 +3,10 @@ import { Image } from 'primereact/image';
 
 export default function NavbarComponent2() {
   return (
-    <div className="flex justify-content-between align-items-center surface-50">
+    <div
+      className="flex justify-content-between align-items-center surface-50"
+      style={{ overflow: 'hidden' }}
+    >
       <div
         className="flex align-items-center w-3"
         style={{ marginLeft: '30px' }}
@@ -13,14 +16,22 @@ export default function NavbarComponent2() {
       <div className="flex align-items-center justify-content-end w-9 flex-nowrap">
         <a href="http://localhost:5173/SignIn" className="mx-2">
           <Button
-            style={{ minWidth: '120px', whiteSpace: 'nowrap' }}
+            style={{
+              minWidth: '120px',
+              maxWidth: '150px',
+              whiteSpace: 'nowrap',
+            }}
             className="py-3 px-5 text-white"
             label="Entrar"
           />
         </a>
         <a href="http://localhost:5173/signUp" className="mx-2">
           <Button
-            style={{ minWidth: '120px', whiteSpace: 'nowrap' }}
+            style={{
+              minWidth: '120px',
+              maxWidth: '150px',
+              whiteSpace: 'nowrap',
+            }}
             className="py-3 px-5 text-white"
             label="Cadastre-se"
           />
