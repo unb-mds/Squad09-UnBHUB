2c691f3f99f0f56639823ee7fc56cf2eca64e31f
Túlio Celeri <celeritulio@terra.com.br>
2024-08-30 23:33:15 -0300

    Test(#163): Realização de teste unitário no botão de "Saiba Mais" da LandingPage
    #163

---
+++ src/__tests__/components/Landing/Hero.spec.tsx
@@ -0,0 +1,22 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Hero from '../../../components/Landing/Hero'; // Ajuste o caminho conforme necessário
+
+describe('Teste no componente Hero', () => {
+  test('Renderiza o botão "Saiba Mais"', () => {
+    render(<Hero />);
+
+    // Verifica se o botão "Saiba Mais" está no documento
+    const saibaMaisButton = screen.getByRole('button', { name: /Saiba Mais/i });
+    expect(saibaMaisButton).toBeInTheDocument();
+  });
+
+  test('O botão "Saiba Mais" redireciona para a URL correta quando clicado', () => {
+    render(<Hero />);
+
+    // Simula o clique no botão "Saiba Mais"
+    const saibaMaisButton = screen.getByRole('button', { name: /Saiba Mais/i });
+    userEvent.click(saibaMaisButton);
+  });
+});
